7236a86e4349bd13f8787b556c88da936c951e92
prot0s34 <nikprotosmas@yandex.ru>
2023-11-16 02:50:02 +0300

    WIP

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,8 +1,8 @@
 #Basics
 #alias ls="exa --icons --group-directories-first"
 #alias ll="exa -l --icons --no-user --git -s time"
-alias vi='nvim'
-alias vim='nvim'
+alias vi='lvim'
+alias vim='lvim'
 alias modvim='vi ~/.config/nvim/init.vim'
 alias sshconfig='cat ~/.ssh/config'
 alias modssh='vi ~/.ssh/config'
@@ -15,6 +15,8 @@ alias hosts='ansible-inventory --graph'
 alias grep='grep -i --color'
 alias pip='pip3'
 alias tarz='tar --use-compress-program=pigz'
+alias nano='lvim'
+alias cat='batcat --paging=never' 
 
 # Kubernetes
 alias k='kubectl'
@@ -28,3 +30,4 @@ alias contexts='kubectl config get-contexts'
 alias awsconfig='cat ~/.aws/config'
 alias awslogin='aws sso login; aws configure list-profiles'
 alias awsprofiles='aws configure list-profiles'
+
