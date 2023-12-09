ba41c3d321089da23feb0e81e05ab17a6d8dbe6a
prot0s34 <nikprotosmas@yandex.ru>
2023-12-09 04:43:31 +0300

    upd lvim/config.lua fmtonsave pattern

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -3,7 +3,11 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
-lvim.format_on_save = true
+lvim.format_on_save = {
+  enabled = true,
+  pattern = "*.rs,*.go",
+  timeout = 1000,
+}
 lvim.plugins = {
   { 'instant-markdown/vim-instant-markdown' },
   { 'towolf/vim-helm' },
