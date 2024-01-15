6796615a4644ee6085ee0779dbeadb5f831952a0
prot0s34 <nikprotosmas@yandex.ru>
2024-01-15 12:01:14 +0300

    add wsl clip.exe to lvim config.lua

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -13,3 +13,18 @@ lvim.plugins = {
   { 'towolf/vim-helm' },
   { 'mrjosh/helm-ls' },
 }
+
+if vim.fn.has('wsl') == 1 then
+    vim.g.clipboard = {
+        name = 'WslClipboard',
+        copy = {
+            ['+'] = '/mnt/c/Windows/System32/clip.exe',
+            ['*'] = '/mnt/c/Windows/System32/clip.exe',
+        },
+        paste = {
+            ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+            ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        },
+        cache_enabled = 0,
+    }
+end
